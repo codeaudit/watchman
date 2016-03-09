da35721a81bb765d1ac8444d80eded743c447320
jlueders <justin.lueders@gmail.com>
2016-03-09 09:10:51 -0600

    working on a queue system

--- server/feed-processors/rss.js
+++ server/feed-processors/rss.js
@@ -1,13 +1,17 @@
 "use strict";
 var FeedParser = require('feedparser')
   , request = require('request')
-  , DelayedStream = require('delayed-stream');
+  , DelayedStream = require('delayed-stream')
+  , queue = require('queue');
 
 
 module.exports = class RssFeedProcessor{
 
   constructor(textFeed){
     this.textFeedUrl = textFeed.url;
+    this.queue = queue();
+    this.queue.concurrency=2;
+
   }
 
   startFeed(){
@@ -21,12 +25,11 @@ module.exports = class RssFeedProcessor{
   processFeed(){
     var context = this;
     var feedParser = new FeedParser();
-    feedParser.on('readable',this.processFeedReadable);
+    feedParser.on('readable',function(){this.processFeedReadable(this)});
     request.get(this.textFeedUrl)
       .on('error',function(err) {
         if (err) {
           console.error("error processing feed", context.textFeedUrl, err);
-          return;
         }
       })
       .on('response', function(res) {
@@ -38,22 +41,27 @@ module.exports = class RssFeedProcessor{
             delayed.pipe(feedParser);
           },
           5000);
-      })
+      });
       // .pipe(feedParser);
   }
 
-  processFeedReadable(){
+  processFeedReadable(rssFeedProcessor){
     var item;
     while (item = this.read()) {
-      request.post({
-        url:  "http://localhost:3001/api/extract/process",
-        body:{dataString:item.description},
-        json:true
-      }, function (err) {
-        if (err) {
-          console.error("error creating event:", err);
-        }
+      if(!item || !item.description){return;}
+
+      rssFeedProcessor.queue.push(function() {
+        request.post({
+          url: "http://localhost:3001/api/extract/process",
+          body: {dataString: item.description},
+          json: true
+        }, function (err) {
+          if (err) {
+            console.error("error creating event:", err);
+          }
+        });
       });
     }
+    rssFeedProcessor.queue.start();
   }
 };
