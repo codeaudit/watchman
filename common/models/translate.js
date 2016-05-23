9603d3e5c1295eeb2d28306c71790fff8064a7df
Luke Wendling <luke@lukewendling.com>
2016-05-23 16:37:17 -0500

    add text lang translation endpoints

---
+++ common/models/translate.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const translator = require('../../lib/translator');
+
+module.exports = function(Translate) {
+
+  Translate.remoteMethod(
+    'toEnglish',
+    {
+      description: 'Translate text to English',
+      accepts: {
+        arg: 'args',
+        type: 'object',
+        description: 'object with "text" property',
+        required: true,
+        http: { source: 'body' }
+      },
+      returns: {type: 'object', root: true},
+      http: {path: '/en', verb: 'post'}
+    }
+  );
+
+  Translate.remoteMethod(
+    'detect',
+    {
+      description: 'Detect lang in text',
+      accepts: {
+        arg: 'args',
+        type: 'object',
+        description: 'object with "text" property',
+        required: true,
+        http: { source: 'body' }
+      },
+      returns: {type: 'object', root: true},
+      http: {path: '/detect', verb: 'post'}
+    }
+  );
+
+  Translate.detect = (args, cb) => {
+    translator.detect(args.text, (err, res) => {
+      if (err) return cb(err);
+      cb(null, res);
+    });
+  };
+
+  Translate.toEnglish = (args, cb) => {
+    translator.translateToEnglish(args.text, (err, res) => {
+      if (err) return cb(err);
+      cb(null, res);
+    });
+  };
+};
