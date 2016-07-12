47560f62ecb1217adffcc3e198e195695fc48a94
justinlueders <justin.lueders@gmail.com>
2016-07-12 15:19:35 -0500

    PR #4: caffe image featurizer

---
+++ services/caffe-image-featurizer/caffe_feature_extraction.py
@@ -0,0 +1,143 @@
+import os
+import sys
+import time
+import argparse
+import numpy as np
+import json
+import pandas as pd
+
+from functools import partial
+from caffe_featurizer import CaffeFeaturizer
+
+CAFFE_ROOT = ''
+featurizer = None
+
+def create_dictionary(subdir, file_i):
+    file_full_path = os.path.join(subdir, file_i)
+    dict = {}
+    dict["name"] = file_full_path
+    print '----SERIALIZING----'
+    return json.dumps(dict)
+
+
+def convert_files_to_dictionary(image_dir_path):
+    for subdir, dirs, files in os.walk(image_dir_path):
+        func = partial(create_dictionary, subdir)
+        data = map(func, files)
+        return data
+
+
+def dump(x):
+    return json.dumps(x)
+
+
+def get_caffe_features(file_path):
+    try:
+        featurizer.set_batch_size(1)
+        featurizer.set_files([file_path])
+        featurizer.load_files()
+        featurizer.forward()
+
+        raw = featurizer.featurize()
+        return raw.tolist()
+
+    except Exception as e:
+        print e
+        # print("ERROR reading image file %s" % str(bytes_str))
+        print
+        CURR_NUM_FEATURES = 88
+        sentinel_vec = -1*np.ones(CURR_NUM_FEATURES)
+        return sentinel_vec.tolist()
+
+
+def get_features(image_json_txt_obj):
+    print '---------------PROCESSING IMAGE----------------'
+    image_json_dict = json.loads(image_json_txt_obj)
+    file_path = str(image_json_dict["name"])
+    features = get_caffe_features(file_path)
+    image_json_dict["features"] = features
+    return image_json_dict
+
+
+def file_to_string(output_path):
+    fd = open(output_path)
+    arr = []
+    for line in fd:
+        arr.append(line)
+    return arr
+
+
+def remove_file(file):
+    os.remove(file)
+
+
+def get_all_features_in_path(caffe_root_path, image_dir_path, start_time):
+    if not os.path.isdir(image_dir_path):
+        return None
+
+    global CAFFE_ROOT
+    CAFFE_ROOT = caffe_root_path
+
+    global featurizer
+    featurizer = CaffeFeaturizer(CAFFE_ROOT)
+
+    data_arr = convert_files_to_dictionary(image_dir_path)
+    # submit image rdd to processing
+    rdd_features = map(get_features, data_arr)
+    # save as txt file:
+    output = map(dump, rdd_features)
+
+    print "------------------ %f minutes elapsed ------------------------" % ((time.time() - start_time)/60.0)
+
+    return output
+
+
+# For each image file submitted for processing,
+# saves features and corresponding filename as json object
+# ------------------------------------------
+# NOTE: for each json object in output, check first element of "features" list for -1.
+# If equals -1, then method was unable to extract features from the image
+def run_feature_extraction():
+    start_time = time.time()
+    desc = 'Feature Extraction for Images'
+    parser = argparse.ArgumentParser(
+        description=desc,
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog=desc)
+
+    default_path = 'target_images'
+    parser.add_argument("--input_dir", help="input directory", default=default_path)
+    parser.add_argument("--output", help="output file", default='image_features')
+    parser.add_argument('--caffeRootDir', dest='caffeRootDir', action='store', help='root directory for caffe.')
+    args = parser.parse_args()
+
+    # serialize and put all images in rdd:
+    # use json schema:
+    #     "name": "",
+    #     "bytes": ""
+    #     "features": "[]"
+    image_dir_path = args.input_dir
+    data_arr = convert_files_to_dictionary(image_dir_path)
+
+    global CAFFE_ROOT
+    CAFFE_ROOT = args.caffeRootDir
+
+    sys.path.insert(0, CAFFE_ROOT + 'python')
+
+    global featurizer
+    featurizer = CaffeFeaturizer(CAFFE_ROOT)
+
+    # submit image rdd to processing
+    rdd_features = map(get_features, data_arr)
+    # save as txt file:
+    output = map(dump, rdd_features)
+    with open(args.output, 'w') as outfile:
+      json.dump(output, outfile)
+    print "------------------ %f minutes elapsed ------------------------" % ((time.time() - start_time)/60.0)
+
+    # helper fct to encapsulate:
+    # get_all_features_str(image_dir_path, args.output, start_time)
+
+
+if __name__ == '__main__':
+    run_feature_extraction()
---
+++ services/caffe-image-featurizer/caffe_redis_client.py
@@ -0,0 +1,83 @@
+import redis
+import threading
+import caffe_feature_extraction
+import json
+import time
+
+class Listener(threading.Thread):
+   def __init__(self, r1, r2, channels):
+       threading.Thread.__init__(self)
+       self.redis_1 = r1
+       self.redis_2 = r2
+       self.pubsub_receive = self.redis_1.pubsub()
+       self.pubsub_receive.subscribe(channels)
+       self.caffe_root = '/home/caffe-user/caffe/'
+
+   def work(self, item):
+       #import pdb; pdb.set_trace()
+       key = item['data']
+       if key == 1: # subscribe response
+           print 'SUBSCRIBED BEHAVIOR'
+           return
+       # get object for corresponding item:
+       obj = self.redis_2.hgetall(key)
+       if not obj:
+           self.redis_2.hmset(key, {'state': 'error', 'error': 'could not find item in redis'})
+           print 'COULD NOT FIND ITEM IN REDIS'
+           return
+       print obj
+       print type(obj)
+       # obj_dict = json.loads(obj)
+       obj_dict = obj
+       if obj_dict['state'] != 'downloaded': # not yet ready
+           print 'NOT YET DOWNLOADED'
+           return
+       state = obj_dict['state']
+       path = obj_dict['path']
+       start_time = time.time()
+       output_path = 'temp'
+       image_dir_path = path
+       # try:
+       obj_dict['state'] = 'processing'
+       self.redis_2.hmset(key, obj_dict)
+
+       # get features:
+       print 'GETTING FEATURES'
+       features = caffe_feature_extraction.get_all_features_in_path(self.caffe_root, image_dir_path, start_time)
+       if features == None:
+           print 'INVALID IMAGE DIRECTORY PATH'
+           self.redis_2.hmset(key, {'state': 'error', 'error': 'invalid image directory path'})
+           return
+       print 'FINISHED FEATURE PROCESSING'
+       obj_dict['data'] = features
+       obj_dict['state'] = 'processed'
+       # report features to redis
+       self.redis_2.hmset(key, obj_dict)
+       # except Exception as e:
+       #     print 'ERROR IN FEATURE EXTRACTION'
+       #     obj_dict['state'] = 'error'
+       #     obj_dict['error'] = e
+       #     # report features to redis
+       #     self.redis_2.hmset(key, obj_dict)
+
+       # print item['channel'], ":", item['data']
+
+   def run(self):
+       # listen for messages and do work:
+       for item in self.pubsub_receive.listen():
+           print 'MESSAGE HEARD'
+           if item['data'] == "KILL":
+               self.pubsub_receive.unsubscribe()
+               # self.pubsub_report.unsubscribe()
+               print self, "unsubscribed and finished"
+               break
+           else:
+           # self.pubsub_report.run_in_thread(self.work(item))
+            self.work(item)
+
+if __name__ == "__main__":
+   pool = redis.ConnectionPool(host='redis', port=6379)
+   r1 = redis.Redis(connection_pool=pool)
+   r2 = redis.Redis(connection_pool=pool)
+   client = Listener(r1, r2, ['features'])
+   client.start()
