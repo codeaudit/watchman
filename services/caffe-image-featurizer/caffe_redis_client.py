1854437301d6e69fd22eac21ea8143fd98548fb1
Luke Wendling <luke@lukewendling.com>
2016-07-14 13:38:31 -0500

    fix username

--- services/caffe-image-featurizer/caffe_redis_client.py
+++ services/caffe-image-featurizer/caffe_redis_client.py
@@ -9,7 +9,7 @@ class Worker(threading.Thread):
         threading.Thread.__init__(self)
         self.send = redis_send
         self.item = message
-        self.caffe_root = '/home/jlueders/caffe/'
+        self.caffe_root = '/home/caffe-user/caffe/'
 
     def run(self):
         # import pdb; pdb.set_trace()
