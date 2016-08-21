f128e78f82c0153a804e74eb5657d5bf4e595405
Luke Wendling <luke@lukewendling.com>
2016-08-21 12:07:44 -0500

    improve jobmonitors ui

--- client/modules/core/controllers/job-monitors.ctrl.js
+++ client/modules/core/controllers/job-monitors.ctrl.js
@@ -1,11 +1,40 @@
 angular.module('com.module.core')
   .controller('JobMonitorsCtrl', JobMonitorsCtrl);
 
-function JobMonitorsCtrl($scope, $window, JobMonitor) {
-  $scope.jobMonitors = JobMonitor.find({
-    filter: {
-      limit: 200,
-      order: 'created desc'
-    }
+function JobMonitorsCtrl($scope, $window, JobMonitor, PostsCluster) {
+  var limitOptions = [10, 50, 100, 200];
+
+  angular.extend($scope, {
+    limitOptions: limitOptions,
+    limit: limitOptions[0],
+    totalClustersCount: PostsCluster.count(),
+    changeLimit: changeLimit
   });
+
+  function changeLimit() {
+    JobMonitor.find({
+      filter: {
+        limit: $scope.limit,
+        order: 'start_time desc',
+        include: 'postsClusters'
+      }
+    })
+    .$promise
+    .then(function(jobMonitors) {
+      $scope.jobMonitors = jobMonitors;
+      $scope.clustersCount = jobMonitors
+        .map(getClusters)
+        .reduce(sum, 0);
+    });
+  }
+
+  changeLimit();
+
+  function getClusters(jobMonitor) {
+    return jobMonitor.postsClusters;
+  }
+
+  function sum(prev, curr) {
+    return prev + curr.length;
+  }
 }
