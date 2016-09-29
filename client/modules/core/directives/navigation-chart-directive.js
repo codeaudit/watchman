c6330c83603d4ba4e17a74fa5345a1ac51c4532e
Luke Wendling <luke@lukewendling.com>
2016-09-29 16:40:03 -0500

    ui: display graph axes

--- client/modules/core/directives/navigation-chart-directive.js
+++ client/modules/core/directives/navigation-chart-directive.js
@@ -1,149 +1,169 @@
 'use strict';
+
 angular.module('com.module.core')
-  .directive('navigationChart', [
-    function() {
-      return {
-        link: link,
-        controller: ['$scope', 'ClusterLink','PostsCluster', 'JobMonitor',
-          function($scope, ClusterLink, PostsCluster, JobMonitor) {
-            this.create = create;
+.directive('navigationChart', navigationChartDirective);
 
-            function create(event, callback) {
-              JobMonitor.find({filter:{
-                where: {"featurizer": "linker"}
-              }})
-                .$promise
-                .then(aggregateJobs)
-                .catch(console.error);
-            }
+function navigationChartDirective() {
+  return {
+    controller: navigationChartController,
+    link: link
+  };
 
-            function aggregateJobs(jobs){
-              var minDate,maxDate;
-              var minCount=0, maxCount = 0;
-              var data = [];
-              var finishedCount = 0;
-              jobs.forEach(function(job, iter){
-                var query = {where: {end_time_ms: job.end_time}};
-                ClusterLink.count(query)
-                  .$promise
-                  .then(function(result){
-                    finishedCount++;
-                    if(result.count !=0) {
-                      if (!minDate) {
-                        minDate = job.start_time;
-                        maxDate = job.end_time;
-                      }
-                      minDate = job.start_time < minDate ? job.start_time : minDate;
-                      maxDate = job.end_time > maxDate ? job.end_time : maxDate;
+  function link(scope, elem, attrs, ctrls) {
+    ctrls.create(null, angular.noop);
+  }
+}
 
-                      minCount = result.count < minCount ? result.count : minCount;
-                      maxCount = result.count > maxCount ? result.count : maxCount;
-                    }
-                    data.push({"count": result.count, "date": new Date(job.end_time)});
+function navigationChartController($scope, ClusterLink, JobMonitor) {
+  this.create = create;
 
-                    if(finishedCount==jobs.length){
-                      data.sort(function(a,b){
-                        if (a.date < b.date) {
-                          return -1;
-                        }
-                        if (a.date > b.date) {
-                          return 1;
-                        }
-                        // a must be equal to b
-                        return 0;
-                      });
-                      graphEvents(data, new Date(minDate), new Date(maxDate), minCount, maxCount);
-                    }
-                  })
-                  .catch(console.error);
-
-              })
-            }
-
-            function graphEvents(data, minDate, maxDate, yMin, yMax) {
-              var margin = {top: 0, right: 0, bottom: 0, left: 0};
-
-              var $container = $('.nav-chart-container'),
-                width = $container.width(),
-                height = $container.height();
-
-              var navWidth = width,
-                navHeight = height - margin.top - margin.bottom;
-
-              var navChart = d3.select('.nav-chart-container').classed('chart', true).append('svg')
-                .classed('navigator', true)
-                .attr('width', navWidth + margin.left + margin.right)
-                .attr('height', navHeight + margin.top + margin.bottom)
-                .append('g')
-                .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
-
-              var navXScale = d3.scaleTime()
-                  .domain([minDate, maxDate])
-                  .range([0, navWidth]),
-                navYScale = d3.scaleLinear()
-                  .domain([yMin, yMax])
-                  .range([navHeight, 0]);
-
-              var navXAxis = d3.axisBottom(navXScale);
-
-              navChart.append('g')
-                .attr('class', 'x axis')
-                .attr('transform', 'translate(0,' + navHeight + ')')
-                .call(navXAxis);
-
-              var navData = d3.area()
-                .x(function (d) {
-                  return navXScale(d.date);
-                })
-                .y0(navHeight)
-                .y1(function (d) {
-                  return navYScale(d.count);
-                });
-
-              var navLine = d3.line()
-                .x(function (d) {
-                  return navXScale(d.date);
-                })
-                .y(function (d) {
-                  return navYScale(d.count);
-                });
-
-              navChart.append('path')
-                .attr('class', 'data')
-                .attr('d', navData(data));
-
-              navChart.append('path')
-                .attr('class', 'line')
-                .attr('d', navLine(data));
-
-              var viewport = d3.brushX()
-                .on("end", function () {
-                  redrawChart();
-                });
-
-              function redrawChart() {
-                if(!d3.event.selection){
-                  $scope.dateRangeSelected(0,0);
-                  return;
-                }
-                var start = navXScale.invert( d3.event.selection[0] );
-                var end = navXScale.invert( d3.event.selection[1] );
-                $scope.dateRangeSelected(start.getTime(),end.getTime());
-              }
-
-              navChart.append("g")
-                .attr("class", "viewport")
-                .call(viewport)
-                .selectAll("rect")
-                .attr("height", navHeight);
-
-            }
-
-          }]
-      };
-
-      function link(scope, elem, attrs, ctrls) {
-        ctrls.create(null,function(){});
+  function create(event, callback) {
+    JobMonitor.find({
+      filter: {
+        where: {
+          featurizer: 'linker'
+        }
       }
+    })
+    .$promise
+    .then(aggregateJobs)
+    .catch(console.error);
+  }
 
-    }]);
+  function aggregateJobs(jobs){
+    var minDate,maxDate;
+    var minCount = 0, maxCount = 0;
+    var data = [];
+    var finishedCount = 0;
+    jobs.forEach(function(job) {
+      var query = {where: {end_time_ms: job.end_time}};
+      ClusterLink.count(query)
+        .$promise
+        .then(function(result){
+          finishedCount++;
+          if (result.count != 0) {
+            if (!minDate) {
+              minDate = job.start_time;
+              maxDate = job.end_time;
+            }
+            minDate = job.start_time < minDate ? job.start_time : minDate;
+            maxDate = job.end_time > maxDate ? job.end_time : maxDate;
+
+            minCount = result.count < minCount ? result.count : minCount;
+            maxCount = result.count > maxCount ? result.count : maxCount;
+          }
+          data.push({
+            count: result.count,
+            date: new Date(job.end_time)
+          });
+
+          if (finishedCount == jobs.length){
+            data.sort(function(a,b){
+              if (a.date < b.date) {
+                return -1;
+              }
+              if (a.date > b.date) {
+                return 1;
+              }
+              // a must be equal to b
+              return 0;
+            });
+            graphClusterCounts(data, new Date(minDate), new Date(maxDate), minCount, maxCount);
+          }
+        })
+        .catch(console.error);
+    });
+  }
+
+  function graphClusterCounts(data, minDate, maxDate, yMin, yMax) {
+    var margin = {top: 30, right: 0, bottom: 20, left: 50};
+
+    var $container = $('.nav-chart-container'),
+      width = $container.width(),
+      height = $container.height();
+
+    var navWidth = width,
+      navHeight = height - margin.top - margin.bottom;
+
+    var navChart = d3.select('.nav-chart-container')
+      .classed('chart', true).append('svg')
+      .classed('navigator', true)
+      .attr('width', navWidth + margin.left + margin.right)
+      .attr('height', navHeight + margin.top + margin.bottom)
+      .append('g')
+      .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+    var navXScale = d3.scaleTime()
+        .domain([minDate, maxDate])
+        .range([0, navWidth]);
+
+    var navYScale = d3.scaleLinear()
+        .domain([yMin, yMax])
+        .range([navHeight, 0]);
+
+    var navData = d3.area()
+      .x(function(d) {
+        return navXScale(d.date);
+      })
+      .y0(navHeight)
+      .y1(function(d) {
+        return navYScale(d.count);
+      });
+
+    var navLine = d3.line()
+      .x(function(d) {
+        return navXScale(d.date);
+      })
+      .y(function(d) {
+        return navYScale(d.count);
+      });
+
+    navChart.append('path')
+      .attr('class', 'data')
+      .attr('d', navData(data));
+
+    navChart.append('path')
+      .attr('class', 'line')
+      .attr('d', navLine(data));
+
+    var viewport = d3.brushX()
+      .on('end', function () {
+        redrawChart();
+      });
+
+    var navXAxis = d3.axisBottom(navXScale);
+
+    navChart.append('g')
+      .attr('class', 'x axis')
+      .attr('transform', 'translate(0,' + navHeight + ')')
+      .call(navXAxis);
+
+    var navYAxis = d3.axisLeft(navYScale);
+
+    navChart.append('g')
+      .attr('class', 'y axis')
+      .call(navYAxis)
+    .append('text')
+      .attr('transform', 'rotate(-90)')
+      .attr('y', 6)
+      .attr('dy', '.71em')
+      .style('text-anchor', 'end')
+      .text('clusters');
+
+    function redrawChart() {
+      if(!d3.event.selection){
+        $scope.dateRangeSelected(0,0);
+        return;
+      }
+      var start = navXScale.invert( d3.event.selection[0] );
+      var end = navXScale.invert( d3.event.selection[1] );
+      $scope.dateRangeSelected(start.getTime(),end.getTime());
+    }
+
+    navChart.append('g')
+      .attr('class', 'viewport')
+      .call(viewport)
+      .selectAll('rect')
+      .attr('height', navHeight);
+  }
+}
--- client/modules/core/directives/network-graph-directive.js
+++ client/modules/core/directives/network-graph-directive.js
@@ -1,174 +1,177 @@
 'use strict';
+
 angular.module('com.module.core')
-  .directive('networkGraph', [
-    function() {
-      return {
-        link: link,
-        controller: ['$scope', 'ClusterLink','PostsCluster',
-          function($scope, ClusterLink, PostsCluster) {
+.directive('networkGraph', networkGraphDirective);
 
-            this.create = create;
-            function create(event, start, end, callback) {
-              var query = {
-                filter: {
-                  where: { "end_time_ms": {"between": [start,end]} }
-                }
-              };
-              ClusterLink.find(query)
-                .$promise
-                .then(graphEvents)
-                .then(callback || angular.noop)
-                .catch(console.error);
-            }
+function networkGraphDirective() {
+  return {
+    controller: networkGraphController
+  };
+}
 
-            $scope.loadNetworkGraph = function(start, end) {
-              if($scope.graphSvg)
-                $scope.graphSvg.remove();
-              create(null,start, end, function(){});
-            };
+function networkGraphController($scope, ClusterLink) {
+  var colors = {
+    hashtag: 'Goldenrod',
+    text: 'SteelBlue',
+    image: 'Maroon'
+  };
 
-            var colors = {
-              "hashtag":"Goldenrod",
-              "text":"SteelBlue",
-              "image":"Maroon"
-            };
-            function getNetworkGraph(links){
-              var graph = {};
-              graph.links = links;
-              graph.nodes=[];
-              graph.links.forEach(function(link){
-                link.value = link.weight;
-                graph.nodes.push({"id":link.source, "group":link.source_data_type});
-                graph.nodes.push({"id":link.target, "group":link.target_data_type});
-              });
+  this.create = createGraph;
 
-              graph.nodes = _.uniqWith(graph.nodes, _.isEqual);
-
-              return graph;
-            }
-
-            function graphEvents(links) {
-              var $container = $('.chart-container'),
-                width = $container.width(),
-                height = $container.height(),
-                graph = getNetworkGraph(links),
-                svg = d3.select('.chart-container').append("svg");
-
-              $scope.graphSvg = svg;
-
-              svg.attr("width", '100%')
-                .attr("height", '100%')
-                .attr('viewBox','0 0 '+Math.min(width,height) +' '+Math.min(width,height) )
-                .attr('preserveAspectRatio','xMinYMin')
-                .append("g")
-                .attr("transform", "translate(" + Math.min(width,height) / 2 + "," + Math.min(width,height) / 2 + ")");
-
-              var color = d3.scaleOrdinal(d3.schemeCategory20);
-
-              var zoom = d3.zoom()
-                .scaleExtent([-40, 40])
-                .on("zoom", zoomed);
-
-              var simulation = d3.forceSimulation()
-                .force("link", d3.forceLink().id(function(d) { return d.id; }))
-                .force("charge", d3.forceManyBody())
-                .force("center", d3.forceCenter(width / 2, height / 2));
-
-              var link = svg.append("g")
-                .attr("class", "links")
-                .selectAll("line")
-                .data(graph.links)
-                .enter().append("line")
-                .attr("stroke-width", function (d) {
-                  return Math.sqrt(d.value);
-                });
-
-              var node = svg.append("g")
-                .attr("class", "nodes")
-                .selectAll("circle")
-                .data(graph.nodes)
-                .enter().append("circle")
-                .attr("r", 5)
-                .attr("fill", function (d) {
-                  return colors[d.group];
-                })
-                .on("click",function(d){
-                  $scope.showDetails(d);
-                })
-                .call(d3.drag()
-                  .on("start", dragstarted)
-                  .on("drag", dragged)
-                  .on("end", dragended));
-
-              node.append("title")
-                .text(function (d) {
-                  return d.group;
-                });
-
-              svg.call(zoom);
-
-              function zoomed() {
-                node.attr("transform", d3.event.transform);
-                link.attr("transform", d3.event.transform);
-              }
-
-              simulation
-                .nodes(graph.nodes)
-                .on("tick", ticked);
-
-              simulation.force("link")
-                .links(graph.links);
-
-
-              function ticked() {
-                link
-                  .attr("x1", function (d) {
-                    return d.source.x;
-                  })
-                  .attr("y1", function (d) {
-                    return d.source.y;
-                  })
-                  .attr("x2", function (d) {
-                    return d.target.x;
-                  })
-                  .attr("y2", function (d) {
-                    return d.target.y;
-                  });
-
-                node
-                  .attr("cx", function (d) {
-                    return d.x;
-                  })
-                  .attr("cy", function (d) {
-                    return d.y;
-                  });
-              }
-
-              function dragstarted(d) {
-                if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-                d.fx = d.x;
-                d.fy = d.y;
-              }
-
-              function dragged(d) {
-                d.fx = d3.event.x;
-                d.fy = d3.event.y;
-              }
-
-              function dragended(d) {
-                if (!d3.event.active) simulation.alphaTarget(0);
-                d.fx = null;
-                d.fy = null;
-              }
-
-            }
-
-
-          }]
-      };
-
-      function link(scope, elem, attrs, ctrls) {
-
+  function createGraph(event, start, end, callback) {
+    var query = {
+      filter: {
+        where: {
+          end_time_ms: { between: [start, end] }
+        }
       }
+    };
+    ClusterLink.find(query)
+      .$promise
+      .then(graphClusterLinks)
+      .then(callback || angular.noop)
+      .catch(console.error);
+  }
 
-    }]);
+  $scope.loadNetworkGraph = function(start, end) {
+    if ($scope.graphSvg)
+      $scope.graphSvg.remove();
+
+    createGraph(null, start, end, function(){});
+  };
+
+  function getGraphData(links){
+    var graph = {};
+    graph.links = links;
+    graph.nodes = [];
+    graph.links.forEach(function(link){
+      link.value = link.weight;
+      graph.nodes.push({
+        id: link.source, group: link.source_data_type
+      });
+      graph.nodes.push({
+        id: link.target, group: link.target_data_type
+      });
+    });
+
+    graph.nodes = _.uniqWith(graph.nodes, _.isEqual);
+
+    return graph;
+  }
+
+  function graphClusterLinks(links) {
+    var $container = $('.chart-container'),
+      width = $container.width(),
+      height = $container.height(),
+      graph = getGraphData(links),
+      svg = d3.select('.chart-container').append('svg');
+
+    $scope.graphSvg = svg;
+
+    svg.attr('width', '100%')
+      .attr('height', '100%')
+      .attr('viewBox','0 0 '+Math.min(width,height) +' '+Math.min(width,height) )
+      .attr('preserveAspectRatio','xMinYMin')
+      .append('g')
+      .attr('transform', 'translate(' + Math.min(width,height) / 2 + ',' + Math.min(width,height) / 2 + ')');
+
+    var color = d3.scaleOrdinal(d3.schemeCategory20);
+
+    var zoom = d3.zoom()
+      .scaleExtent([-40, 40])
+      .on('zoom', zoomed);
+
+    var simulation = d3.forceSimulation()
+      .force('link', d3.forceLink().id(function(d) { return d.id; }))
+      .force('charge', d3.forceManyBody())
+      .force('center', d3.forceCenter(width / 2, height / 2));
+
+    var link = svg.append('g')
+      .attr('class', 'links')
+      .selectAll('line')
+      .data(graph.links)
+      .enter().append('line')
+      .attr('stroke-width', function(d) {
+        return Math.sqrt(d.value);
+      });
+
+    var node = svg.append('g')
+      .attr('class', 'nodes')
+      .selectAll('circle')
+      .data(graph.nodes)
+      .enter().append('circle')
+      .attr('r', 5)
+      .attr('fill', function(d) {
+        return colors[d.group];
+      })
+      .on('click',function(d){
+        $scope.showDetails(d);
+      })
+      .call(d3.drag()
+        .on('start', dragstarted)
+        .on('drag', dragged)
+        .on('end', dragended));
+
+    node.append('title')
+      .text(function (d) {
+        return d.group;
+      });
+
+    svg.call(zoom);
+
+    function zoomed() {
+      node.attr('transform', d3.event.transform);
+      link.attr('transform', d3.event.transform);
+    }
+
+    simulation
+      .nodes(graph.nodes)
+      .on('tick', ticked);
+
+    simulation.force('link')
+      .links(graph.links);
+
+
+    function ticked() {
+      link
+        .attr('x1', function(d) {
+          return d.source.x;
+        })
+        .attr('y1', function(d) {
+          return d.source.y;
+        })
+        .attr('x2', function(d) {
+          return d.target.x;
+        })
+        .attr('y2', function(d) {
+          return d.target.y;
+        });
+
+      node
+        .attr('cx', function(d) {
+          return d.x;
+        })
+        .attr('cy', function(d) {
+          return d.y;
+        });
+    }
+
+    function dragstarted(d) {
+      if (!d3.event.active)
+        simulation.alphaTarget(0.3).restart();
+      d.fx = d.x;
+      d.fy = d.y;
+    }
+
+    function dragged(d) {
+      d.fx = d3.event.x;
+      d.fy = d3.event.y;
+    }
+
+    function dragended(d) {
+      if (!d3.event.active) simulation.alphaTarget(0);
+      d.fx = null;
+      d.fy = null;
+    }
+  }
+}
