6bbb5dc9e61384925f264fac045a90f9561842c6
kenglish-pfi <kevin.english@soteradefense.com>
2016-11-07 13:42:55 -0600

    Updated elasticsearch.yml based on version 2.3 example. Put elasticsearch head plugin in additional container layer

--- services/Docker_Build_ES.sh
+++ services/Docker_Build_ES.sh
@@ -1,1 +1,2 @@
-docker build -t "sotera/watchman-elasticsearch:2.6" -f "Dockerfile-ElasticSearch" .
+docker build -t "sotera/watchman-elasticsearch:2.8" -f "Dockerfile-ElasticSearch" .
+docker build -t "sotera/watchman-elasticsearch-head:2.8" -f "Dockerfile-ElasticSearch-Head" .
