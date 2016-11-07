0f080cbe3930ad7cd7d757a443dc7ce2a5031533
kenglish-pfi <kevin.english@soteradefense.com>
2016-11-07 17:08:52 -0600

    Moved Head install back into initial container, added logic for waiting for ES and stopping and starting ES as part of Head install.

--- services/Docker_Build_ES.sh
+++ services/Docker_Build_ES.sh
@@ -1,2 +1,1 @@
-docker build -t "sotera/watchman-elasticsearch:2.8" -f "Dockerfile-ElasticSearch" .
-docker build -t "sotera/watchman-elasticsearch-head:2.8" -f "Dockerfile-ElasticSearch-Head" .
+docker build -t "sotera/watchman-elasticsearch:2.9" -f "Dockerfile-ElasticSearch" .
