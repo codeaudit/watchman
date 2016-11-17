ce7cd7d920129fbe52ada1bf4a57efd6291b22d2
drJAGartner <dr.j.a.gartner@gmail.com>
2016-11-17 14:27:55 -0600

    serialize events for kafka consumption

--- services/dr-manhattan/event_to_kafka.py
+++ services/dr-manhattan/event_to_kafka.py
@@ -13,7 +13,11 @@ def mongo_to_kafka(rec):
                          ]
                 }
 
-    return {'uid':rec['id'],
+    l_rec = []
+    camps = filter(lambda x: x is not None, map(lambda x: x.keys()[0] if x.values()[0]>0.7 else None, rec['campaigns']))
+    for camp in camps:
+        l_rec.append(
+            {'uid':rec['id'],
             'label':rec['name'],
             'startDate': datetime.fromtimestamp(rec['start_time_ms']/1000.0).isoformat(),
             'endDate': datetime.fromtimestamp(rec['end_time_ms']/1000.0).isoformat(),
@@ -23,12 +27,18 @@ def mongo_to_kafka(rec):
             'photos':rec['image_urls'],
             'importanceScore':rec['importance_score'],
             'topicMessageCount':rec['topic_message_count'],
-            'campaigns': rec['campaigns'],
+            'campaigns': camp,
             'newsEventIds':[],
             'location': o_loc}
+        )
+
+    return
 
 def stream_events(l_clusts, kafka_url, kafka_topic):
-    kds = map(lambda x: mongo_to_kafka(x), l_clusts)
+    kds = []
+    for clust in l_clusts:
+        kds.extend(mongo_to_kafka(clust))
+
     producer = KafkaProducer(bootstrap_servers=kafka_url, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
     for doc in kds:
-        producer.send(kafka_topic, doc)
+        state = producer.send(kafka_topic, doc)
--- services/dr-manhattan/mongo_to_kafka.py
+++ services/dr-manhattan/mongo_to_kafka.py
@@ -1,39 +1,5 @@
-from kafka import KafkaProducer
-from datetime import datetime
-import json, requests, argparse
-
-def mongo_to_kafka(rec):
-    loc = sorted(rec['location'], key=lambda x: x['weight'], reverse=True)
-    o_loc = None
-    if len(loc) > 0:
-        o_loc = {"type": "Point",
-                         "coordinates": [
-                             loc[0]["coords"][0]["lng"],
-                             loc[0]["coords"][0]["lat"]
-                         ]
-                }
-    return {'uid':rec['id'],
-            'label':rec['name'],
-            'startDate': datetime.fromtimestamp(rec['start_time_ms']/1000.0).isoformat(),
-            'endDate': datetime.fromtimestamp(rec['end_time_ms']/1000.0).isoformat(),
-            'hashtags': rec['hashtags'],
-            'keywords':rec['keywords'],
-            'urls':rec['urls'],
-            'photos':rec['image_urls'],
-            'importanceScore':rec['importance_score'],
-            'topicMessageCount':rec['topic_message_count'],
-            'campaigns': rec['campaigns'],
-            'newsEventIds':[],
-            'location': o_loc}
-
-def stream_events(l_clusts, kafka_url, kafka_topic):
-    print "Kafka URL:", kafka_url
-    print "Mapping Events"
-    kds = map(lambda x: mongo_to_kafka(x), l_clusts)
-    producer = KafkaProducer(bootstrap_servers=kafka_url, value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-    for doc in kds:
-        print "Event Sent"
-        producer.send(kafka_topic, doc)
+from event_to_kafka import stream_events
+import requests, argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -44,7 +10,6 @@ if __name__ == "__main__":
 
     print "Requesting Events"
     r = requests.get("{}/api/events".format(args.mongo_url)).json()
-
 
     stream_events(r, args.kafka_url, args.kafka_topic)
 
