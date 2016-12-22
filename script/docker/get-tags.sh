f593b61c86d797af41b16ff3af2924b37fe41eac
Luke Wendling <luke@lukewendling.com>
2016-12-22 12:16:40 -0600

    script: list latest tags of docker hub images

---
+++ script/docker/get-tags.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+jq=`which jq`
+
+if ! [ -e "$jq" ]; then
+  echo 'You need to install jq from https://stedolan.github.io/jq/download/'
+  exit $?
+fi
+
+for name in 'aggregate-clustering' 'caffe_redis_client' 'comedian' 'dr-manhattan' \
+'feature-sim' 'image-fetcher' 'ozymandias' 'rorschach'
+do
+  curl -s https://registry.hub.docker.com/v2/repositories/sotera/$name/tags/ |
+  jq --arg name $name '."results"[0] | {name: [$name]} + {last_tag: .name, updated: .last_updated}'
+done
