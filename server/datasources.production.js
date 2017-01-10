c6e4866b63ca19029d310776ada02ad8d66630d2
Luke Wendling <luke@lukewendling.com>
2017-01-10 10:13:39 -0600

    bump aggclustering. add mongo env var

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,7 +2,7 @@ module.exports = {
   db: {
     connector: 'mongodb',
     connectionTimeout: 10000,
-    host: 'mongo',
+    host: process.env.MONGO_PORT || 'mongo',
     port: process.env.MONGO_PORT || 27017,
     database: 'rancor'
   }
