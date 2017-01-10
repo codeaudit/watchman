b25b8a1c5905034f32160b5a1397bc3410f1b58c
Luke Wendling <luke@lukewendling.com>
2017-01-10 10:14:03 -0600

    fix env var

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,7 +2,7 @@ module.exports = {
   db: {
     connector: 'mongodb',
     connectionTimeout: 10000,
-    host: process.env.MONGO_PORT || 'mongo',
+    host: process.env.MONGO_HOST || 'mongo',
     port: process.env.MONGO_PORT || 27017,
     database: 'rancor'
   }
