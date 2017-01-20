d88667300bb4deb12906b749d48dc5279280f50a
Luke Wendling <luke@lukewendling.com>
2017-01-20 08:45:21 -0600

    allow custom db name

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -2,8 +2,8 @@ module.exports = {
   db: {
     connector: 'mongodb',
     connectionTimeout: 10000,
-    host: process.env.MONGO_HOST || 'mongo',
-    port: process.env.MONGO_PORT || 27017,
-    database: 'rancor'
+    host: process.env.DB_HOST || process.env.MONGO_HOST || 'mongo',
+    port: process.env.DB_PORT || process.env.MONGO_PORT || 27017,
+    database: process.env.DB_NAME || 'rancor'
   }
 };
