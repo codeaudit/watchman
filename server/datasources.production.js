ab6e3e7ac419e65a5de61f86cb22a815d4b3f90f
Luke Wendling <luke@lukewendling.com>
2017-02-15 18:02:28 -0800

    cache datasource: fallback to db conf

--- server/datasources.production.js
+++ server/datasources.production.js
@@ -9,8 +9,8 @@ module.exports = {
   cache: {
     connector: 'mongodb',
     connectionTimeout: 10000,
-    host: process.env.CACHE_HOST || 'mongo',
-    port: process.env.CACHE_PORT || 27017,
+    host: process.env.CACHE_HOST || process.env.DB_HOST || process.env.MONGO_HOST || 'mongo',
+    port: process.env.CACHE_PORT || process.env.DB_PORT || process.env.MONGO_PORT || 27017,
     database: process.env.CACHE_NAME || 'cache'
   }
 };
